8f0a8cf7a2c94e1f1de095b87484c7603bcaff80
jeffreydtz <jeffreydietz33@gmail.com>
2021-11-05 16:31:15 -0300

    cambiando archivos react

--- pw-tk/src/App.js
+++ pw-tk/src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import pgimg from './images/torta.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <h1 className="custom-title">DejateTentar</h1>
+        <div className="nav-bar">
+          <h4 className="nav-element nav-hover">Inicio</h4>
+          <h4 className="nav-element nav-hover">Sobre Mi</h4>
+          <h4 className="nav-element nav-hover">Productos</h4>
+          <h4 className="nav-element nav-hover">Cursos</h4>
+          <h4 className="nav-element nav-hover">Mi Perfil</h4>
+        </div>
+        <div className="title-container">
+          <h1 className="custom-title">Bienvenidos</h1>
+          <p className="title-desc">Dejate llevar por los sabores!! </p>
+        </div>
+        <div className="contents-container">
+        <h2>que ofrecemos?</h2>
+        <div className="contens-div">
+          <ul className="contents">
+          <img src={pgimg} className="picture"/>
+            <li className="list-hover">TORTAS</li>
+            <li className="list-hover">TORTAS DE CUMPLEAÑOS</li>
+            <li className="list-hover">ALFAJORES</li>
+            <li className="list-hover">GALLETITAS</li>
+            <li className="list-hover">MASAS SECAS</li>
+            <li className="list-hover">TARTAS</li>
+            <li className="list-hover">CHIPAS</li>
+            <li className="list-hover">BUDINES</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+
+);
 }
 
 export default App;
