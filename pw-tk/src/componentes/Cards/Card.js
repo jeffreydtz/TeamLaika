fdd4f4075590ae17cc0f8bc772df777b692f6efe
jeffreydtz <jeffreydietz33@gmail.com>
2021-11-18 20:24:44 -0300

    explorando las cards

--- pw-tk/src/componentes/Cards/Card.js
+++ pw-tk/src/componentes/Cards/Card.js
@@ -3,16 +3,16 @@ import React from "react";
 
 import "./card.css";
 
-function Card({ imageSource, title, description}) {
+function Card(receta) {
     return (
         <div className="card text-center bg-dark animate__animated animate__fadeInUp">
             <div className="overflow">
-                <img src={imageSource} alt="a wallpaper" className="card-img-top" />
+                <img src={receta.image} alt="a wallpaper" className="card-img-top" />
             </div>
             <div className="card-body text-light">
-                <h4 className="card-title">{title}</h4>
+                <h4 className="card-title">{receta.title}</h4>
                 <p className="card-text text-secondary">
-                    {description}
+                    {receta.description}
                 </p>
             </div>
         </div>
@@ -24,25 +24,3 @@ function Card({ imageSource, title, description}) {
 export default Card;
 
 
-import pgimg from "../images/programacion.jpeg";
-import {Link} from "react-router-dom";
-
-
-function SubjectCard ({subject}) {
-
-    return (
-        <div className="card-container-custom">
-            <div className="card">
-                <img src={pgimg} className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">{subject.name}</h5>
-                    <p className="card-text">
-                        {subject.description}
-                    </p>
-                    <Link to={`/main/courses/detail/${subject.name}`}><a className="btn btn-primary">Ver más</a></Link>
-                </div>
-            </div>
-        </div>
-    )
-}
-export default
