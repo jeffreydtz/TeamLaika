866619979e3be8ddb4ca7ac52a4983285a10c588
kaleal1 <kjlealbonacci@mail.austral.edu.ar>
2021-11-18 05:25:46 -0300

    cambio en algunos códigos

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -9,21 +9,29 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = "__all__"
 
+    def create(self, validated_data):
+        course = super(CourseSerializer, self).create(validated_data)
+        return course
+
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    first_name = serializers.CharField(min_length=5)
 
     class Meta:
         model = get_user_model()
-        fields = ["username", "password"]
+        fields = ["username", "password", "first_name", "last_name"]
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
         )
         return user
 
 class MeSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = "__all__"
+        fields = ["id", "username", "firstname", "last_name"]
--- backend/api/views.py
+++ backend/api/views.py
@@ -13,6 +13,14 @@ from api.serializers import CourseSerializer, RegisterSerializer, MeSerializer
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    # permission_classes = [IsAuthenticated] esto es si quiero autenticar get, post, put
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [IsAuthenticated]
+        else:
+            self.permission_classes = []
+        return super(CourseViewSet, self).get_permissions() #esto es para autenticr solo una
+
 
 
 class RegisterView(generics.CreateAPIView):
@@ -21,5 +29,6 @@ class RegisterView(generics.CreateAPIView):
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def me(request):
-    print(MeSerializer(request.user).data)
-    return Response(MeSerializer(request.user).data, 200)
+    serializer = MeSerializer(request.user)
+    print(serializer.data)
+    return Response(data=serializer.data, status=200)
