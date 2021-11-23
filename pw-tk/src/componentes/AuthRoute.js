a5865ef086dd854979e45c6e316d96a8effe5888
jeffreydtz <jeffreydietz33@gmail.com>
2021-11-23 19:01:44 -0300

    arreglando el login

--- pw-tk/src/componentes/AuthRoute.js
+++ pw-tk/src/componentes/AuthRoute.js
@@ -1,17 +1,29 @@
 
 import {Route, Redirect} from "react-router-dom";
+import {httpGet} from "../utils/httpFunctions";
+import {useEffect, useState} from "react";
 
 
 const AuthRoute = ({exact, children, path}) => {
 
-    const isLoggedIn = localStorage.getItem('token')
+    const [loading, setLoading] = useState(true)
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    useEffect(() => {
+        httpGet('api/me/').then((res) => {
+            setIsLoggedIn(true)
+            setLoading(false)
+        }).catch((error) => {
+            setIsLoggedIn(false)
+            setLoading(false)
+        })
+    },[])
 
     return (
         <Route
             exact={exact}
             path={path}
             render={({ location }) =>
-                isLoggedIn ? (
+                isLoggedIn || loading? (
                     children
                 ) : (
                     <Redirect
